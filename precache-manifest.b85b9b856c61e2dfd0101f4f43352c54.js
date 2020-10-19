d00d60068c7089c6a8ce8155e7e2f0b0e5225c71
mueeznarejo <mueez.dev@gmail.com>
2020-10-19 16:27:20 +0500

    Updates

---
+++ precache-manifest.b85b9b856c61e2dfd0101f4f43352c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e6f625ebbb60c8d05ae4329e7dd18e",
+    "url": "/friends/index.html"
+  },
+  {
+    "revision": "24350dce107d3a0788ef",
+    "url": "/friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a31dfc58e27c4ac77dd4",
+    "url": "/friends/static/css/main.23c074a9.chunk.css"
+  },
+  {
+    "revision": "24350dce107d3a0788ef",
+    "url": "/friends/static/js/2.3a4601fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/friends/static/js/2.3a4601fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31dfc58e27c4ac77dd4",
+    "url": "/friends/static/js/main.0d8ea4e7.chunk.js"
+  },
+  {
+    "revision": "eadcbdaf8c6c16aba0cc",
+    "url": "/friends/static/js/runtime-main.41d79611.js"
+  }
+]);
